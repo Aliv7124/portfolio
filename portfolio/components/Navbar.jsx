b4d553daf7b5bc1ce4a2cafec8a1b09859dad8b2
Aliv7124 <alivroy25@gmail.com>
2025-07-25 12:46:53 +0530

    Update portfolio with latest changes

--- portfolio/components/Navbar.jsx
+++ portfolio/components/Navbar.jsx
@@ -1,29 +1,29 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
 function Navbar() {
   return (
    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" >My Portfolio</a>
+    <Link className="navbar-brand" to="/about">My Portfolio</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/about">About</a>
+          <Link className="nav-link active" aria-current="page" to="/about">About</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/education">Education</a>
+          <Link className="nav-link" to="/education">Education</Link>
         </li>
          <li className="nav-item">
-          <a className="nav-link" href="/skills">Skills</a>
+          <Link className="nav-link" to="/skills">Skills</Link>
         </li>
          <li className="nav-item">
-          <a className="nav-link" href="/projects">Projects</a>
+          <Link className="nav-link" to="/projects">Projects</Link>
         </li>
            <li className="nav-item">
-          <a className="nav-link" href="/contact">Contact</a>
+          <Link className="nav-link" to="/contact">Contact</Link>
         </li>
        </ul>
     </div>
